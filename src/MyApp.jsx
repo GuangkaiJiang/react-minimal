294931f486337bb1b3fd084c940ee8f11c23af2e
Guangkai Jiang <skyrimtravel@outlook.com>
2019-10-10 14:07:46 +0200

    Production bundle analysis

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const App = () => (
-  <h1>Minimal React jiangg</h1>  
+  <h1>Minimal React jiangg
+    <p>Bundle size: 57 bytes, Load time of the bundle: 55 ms, Last commit SHA1: 9302091f064dc4892b63f19626014c9d54326514</p>
+  </h1>  
+  
 )
 
 export default App
