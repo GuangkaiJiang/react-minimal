8a4add87c0579fe91733f69d2e3927892c485a6e
Guangkai Jiang <skyrimtravel@outlook.com>
2019-10-11 14:01:09 +0200

    1.work with multiple JS files [1 point]

---
+++ src/AppTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App2 = () => (
+  <h1>This is another file.If you see this, means it works.
+
+  </h1>  
+  
+)
+
+export default App2
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './MyApp'
-
+import App2 from './AppTitle'
 render(<App />, document.getElementById('reactinjectedhere'))
+render(<App2 />, document.getElementById('reactinjectedhere2'))
